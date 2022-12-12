664efdfc1b8f80539102885c9c85825fbf60bbe7
edmundmagnojr-nabepero <edmund.magno@nabepero.co.jp>
2022-12-12 11:02:50 +0800

    finished one to many relationship

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\User;
+use App\Models\Post;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +18,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function(){
+    $user = User::findOrFail(1);
+    $post = new Post(['title'=>'My first post with Edwin Diaz 2', 'body'=>'I love larvel with Edwin Diaz']);
+    $user->posts()->save($post);
+});
+
+Route::get('/read', function(){
+    $user = User::findOrFail(1);
+    foreach($user->posts as $post){
+        echo $post->title."<br>";
+    }
+    // return $user->posts;
+});
+
+Route::get('/update', function(){
+    $user = User::find(1);
+    $user->posts()->whereId(1)->update(['title'=>'I love laravel', 'body'=>'This is awasome, thank you Edwin Diaz']);
+    $user->posts()->where('id','=','2')->update(['title'=>'I love laravel 2', 'body'=>'This is awasome, thank you Edwin Diaz']);
+});
+
+Route::get('/delete', function(){
+    $user = User::find(1);
+    $user->posts()->whereId(1)->delete();
 });
